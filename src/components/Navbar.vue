75745a2048aca5cd690331f5b9339ddfbc519628
Kamron or Камрон <kamrush6429@gmail.com>
2024-01-08 21:18:05 +0500

    responsive

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,5 +1,13 @@
 <script setup>
 import { RouterLink } from 'vue-router';
+import { ref } from 'vue';
+
+const openMenu = ref(false)
+
+const activeMenu = () => {
+  openMenu.value = !openMenu.value
+  console.log(openMenu.value);
+}
 </script>
 
 <template>
@@ -21,7 +29,14 @@ import { RouterLink } from 'vue-router';
           </p>
         </div>
         <div class="navbar-menu">
-          <ul class="flex gap-8">
+          <div @click="activeMenu" class="burger-menu">
+            <svg xmlns="http://www.w3.org/2000/svg" width="40px" height="40px" viewBox="0 0 24 24" fill="#fff">
+              <path d="M20 7L4 7" stroke="#fff" stroke-width="1.5" stroke-linecap="round" />
+              <path opacity="0.5" d="M20 12L4 12" stroke="#fff" stroke-width="1.5" stroke-linecap="round" />
+              <path d="M20 17L4 17" stroke="#fff" stroke-width="1.5" stroke-linecap="round" />
+            </svg>
+          </div>
+          <ul :class="{ open: openMenu }" class="flex gap-8">
             <li class="menu-text">
               <router-link to="/about">О компании</router-link>
             </li>
@@ -64,32 +79,26 @@ import { RouterLink } from 'vue-router';
           <ul class="flex gap-4">
             <li>
               <router-link class="flex flex-col items-center" to="/">
-                <img src="../assets/img/icons/clock.svg" alt="clock">
-                Статус заказа
-              </router-link>
-            </li>
-            <li>
-              <router-link class="flex flex-col items-center" to="/">
                 <img src="../assets/img/icons/comparison.svg" alt="comparison">
-                Сравнение
+                <p>Сравнение</p>
               </router-link>
             </li>
             <li>
               <router-link class="flex flex-col items-center" to="/">
                 <img src="../assets/img/icons/heart.svg" alt="heart">
-                Избранное
+                <p>Избранное</p>
               </router-link>
             </li>
             <li>
               <router-link class="flex flex-col items-center" to="/">
                 <img src="../assets/img/icons/shopping.svg" alt="shopping">
-                Корзина
+                <p>Корзина</p>
               </router-link>
             </li>
             <li>
               <router-link class="flex flex-col items-center" to="/">
                 <img src="../assets/img/icons/user.svg" alt="user">
-                Войти
+                <p>Войти</p>
               </router-link>
             </li>
           </ul>
@@ -104,6 +113,13 @@ import { RouterLink } from 'vue-router';
   &-header {
     padding: 14px 0;
 
+
+    div {
+      @media (max-width: 1235px) {
+        gap: 0 10px;
+      }
+    }
+
     .nav-text {
       display: flex;
       align-items: center;
@@ -112,40 +128,110 @@ import { RouterLink } from 'vue-router';
       font-style: normal;
       font-weight: 500;
       line-height: normal;
+
+      @media (max-width: 1536px) {
+        font-size: 16px;
+      }
+
+      @media (max-width: 415px) {
+        font-size: 14px;
+      }
+
+      span {
+        @media (max-width: 1536px) {
+          display: none;
+        }
+      }
     }
 
     .navbar-menu {
+      .burger-menu {
+        display: none;
+
+        @media (max-width: 995px) {
+          display: block;
+        }
+      }
+
+      .open {
+        width: 200px;
+        height: 220px;
+        padding: 25px;
+        position: fixed;
+        right: 0;
+        top: 7%;
+        display: flex;
+        justify-content: space-between;
+        flex-direction: column;
+        background-color: #323436;
+        z-index: 10;
+        transition: all 1s;
+
+        li {
+          margin-bottom: 5px;
+        }
+      }
+
       ul {
         @media (max-width: 1645px) {
           gap: 0 10px;
         }
 
+        @media (max-width: 995px) {
+          display: none;
+        }
+
         .menu-text {
           color: #FFF;
           font-size: 20px;
-          font-style: normal;
           font-weight: 450;
-          line-height: normal;
+
+          @media (max-width: 1536px) {
+            font-size: 16px;
+          }
         }
       }
+
     }
   }
 
   &-main {
     .main-container {
+      justify-content: space-between;
       @media (max-width: 1645px) {
         gap: 0 30px;
       }
 
+      @media (max-width: 995px) {
+        flex-wrap: wrap;
+        gap: 15px;
+      }
+
       .search {
+        @media (max-width: 995px) {
+          order: 3;
+          width: 100%;
+        }
+
+
         .catalog-btn {
           padding: 14px 32px;
           color: #FFF;
           font-size: 22px;
-          font-style: normal;
           font-weight: 450;
-          line-height: normal;
           transition: all .4s;
+
+          @media (max-width: 1535px) {
+            padding: 10px 26px;
+          }
+
+          @media (max-width: 1405px) {
+            font-size: 18px;
+          }
+
+          @media (max-width: 740px) {
+            padding: 5px 10px;
+          }
         }
 
         &-block {
@@ -163,6 +249,28 @@ import { RouterLink } from 'vue-router';
             padding: 20px 0px 20px 40px;
             height: 56px;
 
+            @media (max-width: 1645px) {
+              width: 500px;
+            }
+
+            @media (max-width: 1535px) {
+              width: 400px;
+            }
+
+            @media (max-width: 1405px) {
+              height: 50px;
+            }
+
+            @media (max-width: 995px) {
+              width: calc(100vw - 190px);
+            }
+
+            @media (max-width: 740px) {
+              height: 40px;
+              width: calc(100vw - 160px);
+            }
+
+
             &:focus {
               color: #212121;
             }
@@ -172,19 +280,33 @@ import { RouterLink } from 'vue-router';
             position: absolute;
             top: 15px;
             left: 10px;
+
+            @media (max-width: 740px) {
+              top: 9px;
+            }
           }
 
           .search-btn {
             padding: 10px 22px;
             color: #FFF;
             font-size: 22px;
-            font-style: normal;
             font-weight: 450;
-            line-height: normal;
             position: absolute;
-            right: 5px;
-            top: 3.3px;
+            right: 2px;
+            top: 2px;
             transition: all .4s;
+
+            @media (max-width: 1405px) {
+              font-size: 18px;
+            }
+
+            @media (max-width: 592px) {
+              display: none;
+            }
+
+            @media (max-width: 740px) {
+              padding: 5px 10px;
+            }
           }
         }
       }
@@ -197,6 +319,13 @@ import { RouterLink } from 'vue-router';
             font-style: normal;
             font-weight: 450;
             line-height: normal;
+
+            @media (max-width: 1300px) {
+              p {
+                display: none;
+              }
+            }
+
           }
         }
       }
